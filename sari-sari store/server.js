b61f72269b32db78d384dce7dc5a4fc1b9bad508
Ranielo <sultones5@gmail.com>
2024-04-06 11:58:35 +0800

    redo the spacing

--- sari-sari store/server.js
+++ sari-sari store/server.js
@@ -1,25 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Middleware to parse JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const htmlPath = require("./routes/path/htmlPath");
+app.use("/", htmlPath);
 
-
-const htmlPath = require('./routes/path/htmlPath');
-app.use('/', htmlPath);
-
-const middleWare = require('./routes/middleWare/main');
-app.use('/', middleWare);
-
-
+const middleWare = require("./routes/middleWare/main");
+app.use("/", middleWare);
 
 app.use((req, res) => {
   res.status(404).send(`<h1>Error 404: Resource not found</h1>`);
 });
-
-
 
 // Start the server
 const PORT = process.env.PORT || 8080;
