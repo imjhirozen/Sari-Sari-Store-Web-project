68abec76bf41554d8486d1f8d448410551b579f3
LAPTOP ranielo <sultones69@gmail.com>
2024-04-06 11:55:43 +0800

    revert back

--- sari-sari store/server.js
+++ sari-sari store/server.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Middleware to parse JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const htmlPath = require("./routes/path/htmlPath");
-app.use("/page", htmlPath);
 
-const middleWare = require("./routes/middleWare/main");
-app.use("/", middleWare);
+
+const htmlPath = require('./routes/path/htmlPath');
+app.use('/', htmlPath);
+
+const middleWare = require('./routes/middleWare/main');
+app.use('/', middleWare);
+
+
 
 app.use((req, res) => {
   res.status(404).send(`<h1>Error 404: Resource not found</h1>`);
@@ -19,11 +23,6 @@ app.use((req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 8080;
-const HOST = '192.168.86.32'; // change this to your ipv4 ip address
-
-// latop ip 192.168.86.32
-// pc ip 
-
-app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
